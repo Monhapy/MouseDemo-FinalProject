78bbaaf34b48ac7397d2b61104ad5cef7585083d
Berk AYMUTLU <berkaymutluu10@gmail.com>
2024-06-06 08:58:13 +0300

    Bug

--- MouseDemo-Final/Assets/_newGAME/Script/EnemyStateMachine/IyeAttack.cs
+++ MouseDemo-Final/Assets/_newGAME/Script/EnemyStateMachine/IyeAttack.cs
@@ -8,7 +8,8 @@ public class IyeAttack : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            other.GetComponent<Animator>().SetTrigger("Damage");
+            //other.GetComponent<Animator>().SetTrigger("Damage");
+            Debug.Log("oldu");
             
         }
     }
